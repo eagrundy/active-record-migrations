62e182769e57ee51e039f8e97e3eeb2219a92d00
eagrundy <ericamalakian@gmail.com>
2020-11-29 23:33:58 -0800

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,11 @@ Bundler.require
 
 
 # put the code to connect to the database here
-
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
 require_relative "../artist.rb"
+
+# The environment.rb file commonly contains things we want to > read and make available to our Ruby application when it starts. It isn't > necessary that you totally grasp all the parts of this file, but looking through > it with this in mind, you might be able to gather what is happening: some gems, > including active_record are required; something happens with Bundler; our > database connection is established; the artist.rb file is read.
